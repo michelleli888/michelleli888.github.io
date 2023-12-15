4aa5fc60b8f5b6c3e05a4be052cc1a990b740ff2
Michelle Li <michelleli2022@gmail.com>
2023-12-15 14:33:02 -0800

    added photo

---
+++ script.js
@@ -0,0 +1,24 @@
+let infoSign = document.getElementsByClassName('icon-info-sign');
+let extraInfo = document.getElementsByClassName("extra-info");
+var showing=[];
+
+// for(let j=0; j<infoSign.length; j++){
+//     showing.push(false);
+// }
+
+// for (let i = 0; i < infoSign.length; i++){
+//     (function(index){
+//       infoSign[i].addEventListener("click", myScript);
+//       function myScript(){
+//         if(showing[i]==false){
+//             extraInfo[index].style.display = "block";
+//             showing[i] = true;
+//         }
+//         else{
+//             extraInfo[index].style.display = "none";
+//             showing[i] = false;
+//         }
+//       }
+//     })(i);
+// }
+
